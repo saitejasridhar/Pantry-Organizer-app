1b759cb351bc5b1f59aa7809cd87e403a86a4f03
saitejasridhar <63353229+saitejasridhar@users.noreply.github.com>
2020-05-21 13:34:19 +0530

    test button1

---
+++ app/src/main/java/com/example/fridgepal/Freezer.java
@@ -0,0 +1,14 @@
+package com.example.fridgepal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Freezer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_freezer);
+    }
+}
--- app/src/main/java/com/example/fridgepal/MainActivity.java
+++ app/src/main/java/com/example/fridgepal/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.fridgepal;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        button=(Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openfreezer();
+            }
+        });
+    }
+    public void openfreezer()
+    {
+        Intent intent= new Intent(this, Freezer.class);
+        startActivity(intent);
     }
 }
