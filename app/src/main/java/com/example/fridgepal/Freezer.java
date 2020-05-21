4c3e08300849faefdf277d07083812cd8c21ad3a
saitejasridhar <63353229+saitejasridhar@users.noreply.github.com>
2020-05-22 01:49:05 +0530

    With member class

--- app/src/main/java/com/example/fridgepal/Freezer.java
+++ app/src/main/java/com/example/fridgepal/Freezer.java
@@ -9,10 +9,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Freezer extends AppCompatActivity {
-    ImageView imageView;
-    private FirebaseDatabase firebaseDatabase= FirebaseDatabase.getInstance();
-    private DatabaseReference databaseReference=firebaseDatabase.getReference();
-    private DatabaseReference first= databaseReference.child("image");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/fridgepal/Member.java
@@ -0,0 +1,26 @@
+package com.example.fridgepal;
+
+public class Member {
+    String title,image;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Member()
+    {
+
+    }
+}
