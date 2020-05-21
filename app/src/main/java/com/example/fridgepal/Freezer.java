90f8945dde64fb20dc8db49ad1a3be92ce65938d
saitejasridhar <63353229+saitejasridhar@users.noreply.github.com>
2020-05-21 22:30:05 +0530

    test firebase storage

--- app/src/main/java/com/example/fridgepal/Freezer.java
+++ app/src/main/java/com/example/fridgepal/Freezer.java
@@ -3,8 +3,16 @@ package com.example.fridgepal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Freezer extends AppCompatActivity {
+    ImageView imageView;
+    private FirebaseDatabase firebaseDatabase= FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference=firebaseDatabase.getReference();
+    private DatabaseReference first= databaseReference.child("image");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
