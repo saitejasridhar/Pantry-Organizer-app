5d5aa79cab6d26a8d3fbdb94ce5c557f6c9c7469
saitejasridhar <63353229+saitejasridhar@users.noreply.github.com>
2020-05-27 02:37:38 +0530

    Fridge done

--- app/src/main/java/com/example/fridgepal/Fridge.java
+++ app/src/main/java/com/example/fridgepal/Fridge.java
@@ -27,12 +27,22 @@ public class Fridge extends AppCompatActivity implements View.OnClickListener {
                 CardView button2=findViewById(R.id.vegetables);
                 CardView button3=findViewById(R.id.dairy);
                 CardView button4=findViewById(R.id.greens);
+                CardView button5=findViewById(R.id.beverages);
+                CardView button6=findViewById(R.id.herbs);
+                CardView button7=findViewById(R.id.meat);
+                CardView button8=findViewById(R.id.bakery);
+                CardView button9=findViewById(R.id.sauces);
 
 
                 button1.setOnClickListener(this);
                 button2.setOnClickListener(this);
                 button3.setOnClickListener(this);
                 button4.setOnClickListener(this);
+                button5.setOnClickListener(this);
+                button6.setOnClickListener(this);
+                button7.setOnClickListener(this);
+                button8.setOnClickListener(this);
+                button9.setOnClickListener(this);
 
         }
 
@@ -53,6 +63,22 @@ public class Fridge extends AppCompatActivity implements View.OnClickListener {
                         case R.id.greens:
                                 opennext(greens.class);
                                 break;
+                        case R.id.beverages:
+                                opennext(beverages.class);
+                                break;
+                        case R.id.herbs:
+                                opennext(herbs.class);
+                                break;
+                        case R.id.meat:
+                                opennext(meat.class);
+                                break;
+                        case R.id.bakery:
+                                opennext(bakery.class);
+                                break;
+                        case R.id.sauces:
+                                opennext(sauces.class);
+                                break;
+
                 }
         }
 
---
+++ app/src/main/java/com/example/fridgepal/meat.java
@@ -0,0 +1,158 @@
+package com.example.fridgepal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class meat extends AppCompatActivity {
+
+    private RecyclerView mBlogList;
+    private DatabaseReference mDatabase;
+
+    FirebaseUser user;
+    String uid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        user= FirebaseAuth.getInstance().getCurrentUser();
+        assert user != null;
+        uid=user.getUid();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meat);
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("Meat");
+        mDatabase.keepSynced(true);
+
+        mBlogList=(RecyclerView)findViewById(R.id.meatrecycleview);
+        mBlogList.setHasFixedSize(true);
+        mBlogList.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
+    @Override
+    protected void onStart()
+    {
+
+        super.onStart();
+        final MediaPlayer added=MediaPlayer.create(meat.this,R.raw.added);
+        FirebaseRecyclerAdapter<Blog, meat.BlogViewHolder> firebaseRecyclerAdapter=new FirebaseRecyclerAdapter<Blog, meat.BlogViewHolder>
+                (Blog.class,R.layout.blog_row, meat.BlogViewHolder.class,mDatabase )
+        {
+
+            @Override
+            public void populateViewHolder(meat.BlogViewHolder blogViewHolder, final Blog blog, final int i) {
+
+                blogViewHolder.setTitle(blog.getTitle());
+                blogViewHolder.setImage(getApplicationContext(),blog.getImage());
+
+                blogViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final DialogPlus dialog = DialogPlus.newDialog(meat.this)
+                                .setGravity(Gravity.CENTER)
+                                .setMargin(50,0,50,0)
+                                .setContentHolder(new ViewHolder(R.layout.content))
+                                .setExpanded(false)
+                                .create();
+                        dialog.show();
+
+                        View holderView = (LinearLayout)dialog.getHolderView();
+
+                        final EditText title = holderView.findViewById(R.id.quantity);
+                        Button button=holderView.findViewById(R.id.diaadd);
+
+                        button.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+
+                                added.start();
+                                int map= Integer.parseInt(title.getText().toString());
+
+                                FirebaseDatabase.getInstance().getReference().child(uid).child("Fridge").child(getRef(i).getKey()).child("title").setValue(blog.getTitle());
+                                FirebaseDatabase.getInstance().getReference().child(uid).child("Fridge").child(getRef(i).getKey()).child("image").setValue(blog.getImage());
+                                FirebaseDatabase.getInstance().getReference().child(uid).child("Fridge").child(getRef(i).getKey()).child("desc").setValue(blog.getDesc());
+                                FirebaseDatabase.getInstance().getReference().child(uid).child("Fridge").child(getRef(i).getKey()).child("quantity").setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                                Toast.makeText(meat.this,"Item added",Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+
+                    }
+                });
+            }
+        };
+        mBlogList.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+    public static class BlogViewHolder extends RecyclerView.ViewHolder
+    {
+        View mView;
+        Button add;
+
+
+
+        public BlogViewHolder(View itemView)
+        {
+            super(itemView);
+            mView=itemView;
+
+            add=(Button) mView.findViewById(R.id.add);
+            add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+
+        }
+
+        public void setTitle(String title) {
+
+            TextView post_title = (TextView) mView.findViewById(R.id.post_title);
+            post_title.setText(title);
+        }
+        public void setImage(Context ctx, String image)
+        {
+            ImageView post_Image=(ImageView)mView.findViewById(R.id.post_image);
+            Picasso.get().load(image).into(post_Image);
+        }
+
+    }
+};
